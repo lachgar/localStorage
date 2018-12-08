dc79929919b98d0a4eeff32a6e2b0c172ca02540
lachgar <lachgar.m@gmail.com>
2018-12-08 10:05:31 +0100

    header

---
+++ src/java/beans/Employe.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author a
+ */
+public class Employe {
+    private String nom;
+    private String prenom;
+    private String email;
+
+    public Employe(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+    public Employe() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Emlpoye{" + "nom=" + nom + ", prenom=" + prenom + ", email=" + email + '}';
+    }
+    
+    
+    
+}
---
+++ web/script.js
@@ -0,0 +1,68 @@
+var employes = Array();
+var id = 0;
+function load() {
+    if (typeof localStorage !== 'undefined') {
+        if ('employes' in localStorage) {
+            employes = JSON.parse(localStorage.getItem('employes'));
+            var contenu = $('#contenu');
+            var ligne = '';
+            var i = 0;
+            employes.forEach(e => {
+                ligne += '<tr><td>' + e.nom + '</td><td>' + e.prenom + '</td><td>' + e.email + '</td><td><a href="" class="delete" indice="' + i + '">Supprimer</a></td><td><a href="" class="update" indice="' + i + '">Modifier</a></td></tr>';
+                i++;
+            });
+            contenu.html(ligne);
+        }
+    } else {
+        alert("local storage n'est supporté");
+    }
+}
+$(document).ready(function () {
+    load();
+    $('#valider').click(function () {
+        var e = {
+            nom: $('#nom').val(),
+            prenom: $('#prenom').val(),
+            email: $('#email').val()
+        };
+        if ($('#valider').html() === 'Modifier') {
+            employes[id] = e;
+        } else {
+            employes.push(e);
+        }
+        localStorage.setItem("employes", JSON.stringify(employes));
+        load();
+    });
+
+    $('#contenu').on('click', '.delete', function () {
+        id = parseInt($(this).attr('indice'));
+        employes.splice(id, 1);
+        localStorage.removeItem("employes");
+        localStorage.setItem("employes", JSON.stringify(employes));
+
+    });
+    $('#contenu').on('click', '.update', function () {
+        id = parseInt($(this).attr('indice'));
+        $('#nom').val(employes[id].nom);
+        $('#prenom').val(employes[id].prenom);
+        $('#email').val(employes[id].email);
+        $('#valider').html('Modifier');
+        return false;
+    });
+
+    $('#envoyer').click(function (){
+        $.ajax({
+            url: "SendData",
+            data:{employes :JSON.stringify(employes)},
+            type: 'GET',
+            cache: false,
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                
+            }
+        });
+    });
+
+});
